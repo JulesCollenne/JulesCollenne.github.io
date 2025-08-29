798c5929f45387ab312a04822168f207d3942ced
JulesCollenne <jules.collenne@univ-amu.fr>
2025-08-29 17:01:33 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // For a user site (JulesCollenne.github.io), base must be '/'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   base: '/',
 })
 
